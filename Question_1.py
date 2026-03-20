bed59e1adec3ab2a0b05ebf73595543bb891d046
Aditya <adityapaliwalmontu@gmail.com>
2026-03-20 11:27:41 +0530

    pushing the assessment into the github repo

--- Question_1.py
+++ Question_1.py
@@ -11,12 +11,14 @@ driver.maximize_window()
 
 driver.find_element(By.XPATH, '//span[. = "Standings"]').click()
 
+ta_na = driver.find_element(By.XPATH, '(//p[. = "Jaipur Pink Panthers"]/../../../..//p)[3]')
 mpl = driver.find_element(By.XPATH, '(//p[. = "Jaipur Pink Panthers"]/../../../..//p)[4]')
 won = driver.find_element(By.XPATH, '(//p[. = "Jaipur Pink Panthers"]/../../../..//p)[5]')
 lost = driver.find_element(By.XPATH, '(//p[. = "Jaipur Pink Panthers"]/../../../..//p)[6]')
 sc_di = driver.find_element(By.XPATH, '(//p[. = "Jaipur Pink Panthers"]/../../../..//p)[7]')
 pts = driver.find_element(By.XPATH, '(//p[. = "Jaipur Pink Panthers"]/../../../..//p)[13]')
 
+print(f"Name of the Team: {ta_na.text}")
 print(f"Matches Played = {mpl.text}")
 print(f"Won = {won.text}")
 print(f"Lost = {lost.text}")
